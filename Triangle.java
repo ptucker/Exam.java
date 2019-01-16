431a0ef8dadcbcbb9be60a82d0eda4139754e888
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-15 17:48:15 -0800

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,31 @@
+/**
+* This class models a triangle
+* @author Karen Sobtafo
+* @version 1.00, 15 January 2019
+*/
+public class Triangle implements Shape {
+    public String Kind;
+    public int ID;
+    public int side1;
+    public int side2;
+    public int side3;
+    public String color;
+  
+  
+    public String toString()
+    {
+        return " ";
+    }
+   public String getKind()
+    {
+        return  " ";
+    }
+   public String getDetailString()
+    {
+        return " ";
+    }
+    public int getID()
+    {
+        return 5;
+    }
+}
