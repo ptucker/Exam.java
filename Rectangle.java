2d83261299d6c3590684393764577f76dd13ffab
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-15 17:47:49 -0800

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,31 @@
+/**
+* This class models a rectangle
+* @author Karen Sobtafo
+* @version 1.00, 15 January 2019
+*/
+public class Rectangle implements Shape {
+    public String Kind;
+    public int ID;
+    public int width;
+    public String color;
+    public int length;
+
+    
+    public String toString()
+    {
+        return " ";
+    }
+   public String getKind()
+    {
+        return  " ";
+    }
+   public String getDetailString()
+    {
+        return " ";
+    }
+    public int getID()
+    {
+        return 5;
+    }
+
+}
