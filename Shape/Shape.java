2317ae6d51dbf30bcf6742a924db97a815134d28
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-15 17:45:39 -0800

    Create Shape.java

---
+++ Shape/Shape.java
@@ -0,0 +1,18 @@
+
+/**
+* This interface models the characteristics of a shape
+* @author Karen Sobtafo
+* @version 1.00, 15 January 2019
+*/
+public interface Shape {
+
+    String toString();
+    //return the kind and ID of the shape 
+    String getKind();
+    //return the kind of shape 
+    String getDetailString();
+    //return the details of that shape (including kind, ID, properties, area, perimeter) –  displayed 
+    int getID();
+    //return the ID of the shape
+}
+
