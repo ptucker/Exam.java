c8465f81f935ec71ec6f21c0efa78e312637c53c
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-15 21:36:15 -0800

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -3,11 +3,13 @@
 * @author Karen Sobtafo
 * @version 1.00, 15 January 2019
 */
+import java.util.ArrayList;
 public class Circle implements Shape 
 {
+    public ArrayList<Shape> shapes = new ArrayList<Shape>();
     public String Kind;
-    public int ID;
-    public int radius;
+    public String ID;
+    public String radius;
     public String color;
 
     public String toString()
