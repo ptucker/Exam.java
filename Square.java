131a5d9ea519caeea70a729fcedb3e0108861b1a
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-15 17:47:32 -0800

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,30 @@
+/**
+* This class models a square
+* @author Karen Sobtafo
+* @version 1.00, 15 January 2019
+*/
+public class Square implements Shape {
+    public String Kind;
+    public int ID;
+    public int side;
+    public String color;
+   
+   
+    public String toString()
+    {
+        return " ";
+    }
+   public String getKind()
+    {
+        return  " ";
+    }
+   public String getDetailString()
+    {
+        return " ";
+    }
+    public int getID()
+    {
+        return 5;
+    }
+
+}
